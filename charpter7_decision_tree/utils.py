d6db762e22cbb0f56003ae37db01fa9b5d5c3535
louwill <ygnjd2016@gmail.com>
2021-05-11 22:46:19 +0800

    Add files via upload

---
+++ charpter7_decision_tree/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+### 定义二叉特征分裂函数
+def feature_split(X, feature_i, threshold):
+    split_func = None
+    if isinstance(threshold, int) or isinstance(threshold, float):
+        split_func = lambda sample: sample[feature_i] >= threshold
+    else:
+        split_func = lambda sample: sample[feature_i] == threshold
+
+    X_left = np.array([sample for sample in X if split_func(sample)])
+    X_right = np.array([sample for sample in X if not split_func(sample)])
+
+    return np.array([X_left, X_right])
+
+### 计算基尼指数
+def calculate_gini(y):
+    y = y.tolist()
+    probs = [y.count(i)/len(y) for i in np.unique(y)]
+    gini = sum([p*(1-p) for p in probs])
+    return gini
